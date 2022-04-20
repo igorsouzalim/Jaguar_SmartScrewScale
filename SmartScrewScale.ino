509f34b6c689e836f4b1197e77e7c6004a10f294
Igor <igorsouzalim@gmail.com>
2022-04-20 16:19:50 -0300

    Teste 1 sucesso

--- SmartScrewScale.ino
+++ SmartScrewScale.ino
@@ -6,7 +6,8 @@
 #include <ThingerESP32.h>
 
 
-#define totalParafusos 30 //editar conforme qtd utilizada
+#define totalParafusos 110 //editar conforme qtd utilizada
+#define LED 36 //quantidade de leds na fita
 //Thingerio
 #define USERNAME "LaboratorioAberto"
 #define DEVICE_ID "jaguarEsp32"
@@ -22,8 +23,8 @@
 
 //Fita de LED
 #define PIN        4 // On Trinket or Gemma, suggest changing this to 1
-#define NUMPIXELS 20 // Popular NeoPixel ring size
-#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
+#define NUMPIXELS 36 // Popular NeoPixel ring size
+#define BRIGHTNESS 255 // Set BRIGHTNESS to about 1/5 (max = 255)
 
 #define LOADCELL_DOUT_PIN 18
 #define  LOADCELL_SCK_PIN 5
@@ -62,11 +63,17 @@ void setup() {
   if (pesoMin == 0){
     Serial.println("No values saved for pesoMin");
   }
+  else
+  Serial.println("PesoMin = " + String(pesoMin));
 
   pesoMax = preferences.getLong("pesoMax", 0);
-  if (pesoMin == 0){
+  if (pesoMax == 0){
     Serial.println("No values saved for pesoMax");
   }
+  else
+  Serial.println("pesoMax = " + String(pesoMax));
+
+
 
   //LoadCell begin
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
@@ -74,8 +81,25 @@ void setup() {
   //LED begin
   pixels.begin(); 
 
+   pixels.clear();  // turn off LEDs
+
+
+  for(int j=0; j<36; j++)  
+  {
+    pixels.setPixelColor(j, pixels.Color(50, 230, 50)); 
+    pixels.show(); 
+    delay(100);
+  }
+  for(int j=36; j>0; j--)  
+  {
+    pixels.setPixelColor(j, pixels.Color(150, 0, 150)); 
+    pixels.show(); 
+    delay(100);
+  }
+
+  
   //Inicialization effect
-  for(int i=0 ; i<10; i++)
+  for(int i=0 ; i<2; i++)
   startLed();
 
   //setup thingerio connection
@@ -106,8 +130,12 @@ if(initCalibration == 0)
   
   readPeso = scale.read_average(10);
 
+  if(readPeso < pesoMin)
+  readPeso = pesoMin;
+  else if(readPeso > pesoMax)
+  readPeso = pesoMax;
   
-  nLeds = map(readPeso, pesoMin, pesoMax, 0, 40);
+  nLeds = map(readPeso, pesoMin, pesoMax, 0, LED);
   percent = map(readPeso, pesoMin, pesoMax, 0, 100);
   estimated = map(readPeso, pesoMin, pesoMax, 0, totalParafusos);
   chart1 = estimated;
@@ -123,13 +151,13 @@ if(initCalibration == 0)
   scaleTaskExecuting = 0;
 
   scale.power_down();			        // put the ADC in sleep mode
-  vTaskDelay( 100/ portTICK_PERIOD_MS ); 
+  vTaskDelay( 20/ portTICK_PERIOD_MS ); 
   scale.power_up();
 
-}
 
 
-/*
+
+
 if(nLeds==0)  // standby LED
 {
   StandbyLedEffect();
@@ -143,7 +171,9 @@ else{           // displays the LED with peso value
   }
   pixels.show(); 
 }
-*/
+
+}
+
 }
 
 
@@ -158,17 +188,7 @@ void vTaskThingerio(void *pvParameters)
     //{Serial.println("scaleTaskExecuting -------------- waiting for the end of the task... ");}
 
 
-    if(statusCount >= 100)
-    {
-      
-      status_signal++;
-      if(status_signal >1)
-      status_signal = 0;
-
-      statusCount = 0;
-    }
-    
-    statusCount++;
+    status_signal = 1;
 
     vTaskDelay( 10/ portTICK_PERIOD_MS ); 
   }
@@ -199,17 +219,26 @@ void vTaskCalibration(void *pvParameters)
       reedSwitchCount--;
       pixels.clear(); 
       pixels.show(); 
+      initCalibration = 0;
       }
     }
 
-    if(reedSwitchCount == 18)  // Entra no processo de calibracao
+    if(reedSwitchCount > 0)
+    initCalibration = 1;
+   
+
+
+    if(reedSwitchCount == 36)  // Entra no processo de calibracao
     {
+
+      Serial.println("Entra Calibracao");
+
       initCalibration = 1;
 
-      for(int i = 0;i<20;i++) pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // apaga Leds
+      for(int i = 0;i<LED;i++) pixels.setPixelColor(i, pixels.Color(0, 0, 0)); // apaga Leds
       pixels.show();   vTaskDelay( 300/ portTICK_PERIOD_MS ); 
 			     
-      for(int i = 0;i<20;i++) pixels.setPixelColor(i, pixels.Color(150, 0, 0));
+      for(int i = 0;i<LED;i++) pixels.setPixelColor(i, pixels.Color(150, 0, 0));
       pixels.show();    vTaskDelay( 300/ portTICK_PERIOD_MS ); 
 
       pesoMin = scale.read_average(50);  // Le o peso do prato
@@ -247,7 +276,7 @@ void vTaskCalibration(void *pvParameters)
 
 
 
-    vTaskDelay( 100/ portTICK_PERIOD_MS );
+    vTaskDelay( 50/ portTICK_PERIOD_MS );
   }
 }
 
@@ -257,19 +286,19 @@ void calibrationLedEffect_1()
 
   delay(500);
 
-  for(int i=0; i<10; i++) { 
+  for(int i=0; i<=18; i++) { 
     
     pixels.setPixelColor(i, pixels.Color(150, 0, 150));
-    pixels.setPixelColor(19-i, pixels.Color(150, 0, 150));
+    pixels.setPixelColor(LED-i, pixels.Color(150, 0, 150));
     pixels.show();   
 
     delay(50); 
   }
 
-  for(int i=9; i>=0; i--) { 
+  for(int i=18; i>=0; i--) { 
     
     pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-    pixels.setPixelColor(19-i, pixels.Color(0, 0, 0));
+    pixels.setPixelColor(LED-i, pixels.Color(0, 0, 0));
     pixels.show();   
 
     delay(1200); 
@@ -282,19 +311,19 @@ void startLed()
 
   delay(500);
 
-  for(int i=0; i<10; i++) { 
+  for(int i=0; i<18; i++) { 
     
     pixels.setPixelColor(i, pixels.Color(150, 0, 150));
-    pixels.setPixelColor(19-i, pixels.Color(150, 0, 150));
+    pixels.setPixelColor(LED-i, pixels.Color(150, 0, 150));
     pixels.show();   
 
     delay(50); 
   }
 
-  for(int i=9; i>=0; i--) { 
+  for(int i=17; i>=0; i--) { 
     
     pixels.setPixelColor(i, pixels.Color(0, 150, 0));
-    pixels.setPixelColor(19-i, pixels.Color(0, 150, 0));
+    pixels.setPixelColor(LED-i, pixels.Color(0, 150, 0));
     pixels.show();   
 
     delay(50); 
@@ -303,11 +332,14 @@ void startLed()
 
 void calibrationLedEffect_2()
 {
-  for(int i = 0;i<20;i++) pixels.setPixelColor(i, pixels.Color(150, 0, 150));
+  pixels.clear(); 
+  pixels.show();
+
+  for(int i = 0;i<LED;i++) pixels.setPixelColor(i, pixels.Color(150, 0, 150));
   pixels.show(); vTaskDelay( 300/ portTICK_PERIOD_MS ); 
-  for(int i = 0;i<20;i++) pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+  for(int i = 0;i<LED;i++) pixels.setPixelColor(i, pixels.Color(0, 0, 0));
   pixels.show(); vTaskDelay( 300/ portTICK_PERIOD_MS ); 
-  for(int i = 0;i<20;i++) pixels.setPixelColor(i, pixels.Color(150, 0, 150));
+  for(int i = 0;i<LED;i++) pixels.setPixelColor(i, pixels.Color(150, 0, 150));
   pixels.show();  vTaskDelay( 300/ portTICK_PERIOD_MS ); 
 }
 
@@ -316,13 +348,13 @@ void StandbyLedEffect()
 {
   pixels.clear(); 
       
- for(int i = 0;i<20;i++) pixels.setPixelColor(i, pixels.Color(200, 0, 0)); pixels.show();  // apaga Leds
+ for(int i = 0;i<36;i++) pixels.setPixelColor(i, pixels.Color(200, 0, 0)); pixels.show();  // apaga Leds
  delay(200);
- for(int i = 0;i<20;i++) pixels.setPixelColor(i, pixels.Color(0, 0, 0)); pixels.show();  // apaga Leds
+ for(int i = 0;i<36;i++) pixels.setPixelColor(i, pixels.Color(0, 0, 0)); pixels.show();  // apaga Leds
  delay(100);
- for(int i = 0;i<20;i++) pixels.setPixelColor(i, pixels.Color(200, 0, 0)); pixels.show();  // apaga Leds
+ for(int i = 0;i<36;i++) pixels.setPixelColor(i, pixels.Color(200, 0, 0)); pixels.show();  // apaga Leds
  delay(200);
- for(int i = 0;i<20;i++) pixels.setPixelColor(i, pixels.Color(0, 0, 0)); pixels.show();  // apaga Leds
+ for(int i = 0;i<36;i++) pixels.setPixelColor(i, pixels.Color(0, 0, 0)); pixels.show();  // apaga Leds
  delay(350);
  
  
